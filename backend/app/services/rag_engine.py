373371b8ce0b1226940853d1e4853dfd96cad524
drema3-4 <drema3-4>
2026-02-25 01:31:16 +0300

    feature-beckend-init-Rag-and-API: добавил prompts.py и логику с промптами

--- backend/app/services/rag_engine.py
+++ backend/app/services/rag_engine.py
@@ -1,5 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from ..core.config import settings
+from langchain_core.prompts import PromptTemplate
+from ..core.prompts import PROMPT_TEMPLATE, PROMPT_TEMPLATE_INPUT_VARIEBLES
 from langchain_classic.chains.retrieval_qa.base import RetrievalQA
 
 
@@ -9,11 +11,19 @@ class RAGEngine:
     ):
         self.retriever = retriever
         self.llm_settings = settings.llm
+        self.prompt = PromptTemplate(
+            template=PROMPT_TEMPLATE,
+            input_variables=PROMPT_TEMPLATE_INPUT_VARIEBLES
+        )
         self.__init_chain__()    
 
     def __init_chain__(self):
         llm = ChatGoogleGenerativeAI(**self.llm_settings.to_langchain_params())
-        self.chain = RetrievalQA.from_chain_type(llm=llm, retriever=self.retriever)
+        self.chain = RetrievalQA.from_llm(
+            llm=llm,
+            retriever=self.retriever,
+            prompt=self.prompt
+        )
 
     def query(self, question):
         return self.chain.run(question)
